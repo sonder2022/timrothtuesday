daf31c2ddd11346e4f3287e16240fa4f60cd709f
sonder2022 <jamielinnlefort@gmail.com>
2026-04-15 11:56:24 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -34,8 +34,5 @@ $(function() {
     // Populate using a JSON file
     // WTF.init( 'sample.json' );
 
-    // Populate using a Google spreadsheet ID (you must publish it first!)
-    // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc' );
 
 });
